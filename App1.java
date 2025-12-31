52d7ebfa5475133065933d5abdd850038f04016d
Vladimir Gorin <vladimirgorin410@gmail.com>
2025-12-31 20:04:51 +0600

    traning

--- App1.java
+++ App1.java
@@ -3,19 +3,21 @@ public class App1 {
         int years = 4;
         int monthDate = 29;
 
-        if(monthDate != 29){
-            System.out.println("Ошибка дата юбилея должна быть 29");
+        if (monthDate != 29) {
+            System.out.println("Ошибка monthDate должна быть 29");
+            return;
+        }
+
+        if (years < 4) {
+            System.out.println("Ошибка years должен быть не меньше 4");
         }
 
         switch (years) {
-            case 4 -> System.out.println("Количество юбилеев: " + 1);
-
-            case 7 -> System.out.println("Количество юбилеев: " + 1);
+            case 4, 7 -> System.out.println("Количество юбилеев: " + 1);
 
             case 8 -> System.out.println("Количество юбилеев: " + 2);
 
-            default -> {
-            }
+            default -> System.out.println("Количество юбилеев: 0");
         }
     }
 }
